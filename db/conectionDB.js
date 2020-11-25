35ea522cd6047838e1b41d6e62de7ce5d8b6ff98
David M Ramirez <davidramirez@MacBook-Pro-de-David.local>
2020-11-25 10:25:38 -0500

    modificaciones en profile mostrar datos dinamicos

--- db/conectionDB.js
+++ db/conectionDB.js
@@ -1,6 +1,7 @@
 const { Client } = require('pg');
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
+    // connectionString: "postgres://mtburmilevccie:1c22b8d4b0908a9819c5d4593efdb9ced5202aceab2977a6e426fcac3ce4df58@ec2-34-237-236-32.compute-1.amazonaws.com:5432/d3qj9alp37ad76",
     ssl: {
         rejectUnauthorized: false
     }
@@ -10,7 +11,6 @@ client.connect()
     .then(() => console.log('connected'))
     .catch(err => console.error('connection error', err.stack));
 
-
 const resultQuery = async(query) => {
     return await client.query(query)
         .then(res => {
@@ -18,10 +18,5 @@ const resultQuery = async(query) => {
         })
         .catch(e => console.error(e.stack))
 }
-
-
-
-
-
 
 module.exports = { resultQuery, client }
--- routes/index.js
+++ routes/index.js
@@ -15,17 +15,36 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/profile', requiresAuth(), function(req, res, next) {
-    console.log(req.oidc.user);
-    const dataQ = dataQuery.resultQuery('SELECT * from estudiante').then((data) => {
+    // capturar email del Auth0 para usarlo en el query
+    console.log(req.oidc.user.email);
+    const mail = req.oidc.user.email;
+    const dataQ = dataQuery.resultQuery(`SELECT * from estudiante WHERE email = '${mail}' ;`).then((data) => {
+            // const dataQ = dataQuery.resultQuery(`SELECT * from estudiante ;`).then((data) => {
+           
+            // stringify recibe el data y lo transforma en un json de tipo string
+            let datos = JSON.stringify(data, null, 2)
+
+            // convertir de string a json:
+            var datos2 = JSON.parse(datos);
+            // console.log('los datos son');
+            // console.log(datos2);
+            // capturar solo nombre
+            // console.log('nombre: ');
+            // console.log(datos2[0].nombre);
+
             res.render('profile', {
                 userProfile: JSON.stringify(req.oidc.user, null, 2),
                 title: 'Perfil',
-                data: data
+                nombre: datos2[0].nombre,
+                apellido: datos2[0].apellido,
+                numIdentidad: datos2[0].numIdentidad,
+                carrera: datos2[0].carrera,
+                puntos_Acum: datos2[0].puntos_Acum
             });
+            
         })
         .catch((err) => console.log(err));
 });
-
 
 
 
