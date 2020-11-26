9c57588c9927d64af5f56787cf71ba24650a370e
EdwardVelasquez <edwardvelasquez3113j@hotmail.com>
2020-11-26 17:47:29 -0500

    Actualización del perfil del estudiante

--- db/conectionDB.js
+++ db/conectionDB.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    // connectionString: "postgres://mtburmilevccie:1c22b8d4b0908a9819c5d4593efdb9ced5202aceab2977a6e426fcac3ce4df58@ec2-34-237-236-32.compute-1.amazonaws.com:5432/d3qj9alp37ad76",
+    //connectionString: process.env.DATABASE_URL,
+    connectionString: "postgres://mtburmilevccie:1c22b8d4b0908a9819c5d4593efdb9ced5202aceab2977a6e426fcac3ce4df58@ec2-34-237-236-32.compute-1.amazonaws.com:5432/d3qj9alp37ad76",
     ssl: {
         rejectUnauthorized: false
     }
--- routes/index.js
+++ routes/index.js
@@ -15,33 +15,45 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/profile', requiresAuth(), function(req, res, next) {
-    // capturar email del Auth0 para usarlo en el query
+    const elementActividades = {}
+        // capturar email del Auth0 para usarlo en el query
     console.log(req.oidc.user.email);
     const mail = req.oidc.user.email;
-    const dataQ = dataQuery.resultQuery(`SELECT * from estudiante WHERE email = '${mail}' ;`).then((data) => {
+    const dataQ = dataQuery.resultQuery(`SELECT estudiante.puntos_acum ,estudiante.nombre ,estudiante.numidentidad,estudiante.carrera,estudiante.apellido,actividad.desc_actividad ,actividad.puntos_actividad, participacion.fecha_participacion from estudiante inner join participacion on participacion.id_estudiante =estudiante.id_estudiante
+    inner join actividad on actividad.id_actividad = participacion.id_actividad WHERE estudiante.email= '${mail}' ;`).then((data) => {
             // const dataQ = dataQuery.resultQuery(`SELECT * from estudiante ;`).then((data) => {
-           
+            console.log(data);
             // stringify recibe el data y lo transforma en un json de tipo string
-            let datos = JSON.stringify(data, null, 2)
+            let datos = JSON.stringify(data, null, 2);
 
             // convertir de string a json:
             var datos2 = JSON.parse(datos);
+            for (const key in datos2) {
+                if (datos2.hasOwnProperty(key)) {
+                    elementActividades[`Actividad${key}`] = datos2[key].desc_actividad;
+                    elementActividades[`Puntos${key}`] = datos2[key].puntos_actividad;
+
+                }
+            }
+            console.log('const', elementActividades);
+
             // console.log('los datos son');
             // console.log(datos2);
             // capturar solo nombre
             // console.log('nombre: ');
             // console.log(datos2[0].nombre);
-
             res.render('profile', {
                 userProfile: JSON.stringify(req.oidc.user, null, 2),
                 title: 'Perfil',
                 nombre: datos2[0].nombre,
                 apellido: datos2[0].apellido,
-                numIdentidad: datos2[0].numIdentidad,
+                numIdentidad: datos2[0].numidentidad,
                 carrera: datos2[0].carrera,
-                puntos_Acum: datos2[0].puntos_Acum
+                puntos_Acum: datos2[0].puntos_acum,
+                actividad: datos2[1].desc_actividad,
+                fecha: datos2[0].fecha_participacion,
+                puntosActividad: datos2[0].puntos_actividad
             });
-            
         })
         .catch((err) => console.log(err));
 });
