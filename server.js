4f89a18be12d497f00ff6662f13784fe93ae99c8
David M Ramirez <davidramirez@MacBook-Pro-de-David.local>
2021-08-28 09:57:01 -0300

    commit 1

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const config = {
   auth0Logout: true
 };
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
   config.baseURL = `http://localhost:${port}`;
 }
